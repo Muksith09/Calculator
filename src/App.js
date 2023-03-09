dae91bf6961a0cf692f8faf2b862380bf9f35ca5
muksith <muksithmajeed@gmail.com>
2023-03-09 23:09:52 +0530

    calculator project completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [output, setOutput] = useState("");
+  const [result, setResult] = useState("");
+  const operators = ['/', '*', '+', '-', '.'];
+
+  const updateOutput = (value)=>{
+    if( (operators.includes(value) && output === '') ||
+        (operators.includes(value) && operators.includes(output.slice(-1)))
+    ){
+      return ;
+    }
+    setOutput(output + value)
+    if(!operators.includes(value)){
+      setResult(eval(output + value).toString())
+    }
+  }
+  const showResult = () => {
+    setOutput(eval(output).toString())
+  }
+  const deleteOutput = ()=>{
+      if( output ==='')
+      { return}
+      const value = output.slice(0, -1)
+      setOutput(value) 
+  }
+  const answer=()=>{
+    setOutput(output + result)
+    setResult(eval(output + result).toString())
+  }
+   const clear =()=>{
+    setOutput('')
+    setResult('')
+   }
+
+  const createDigits = ()=>{
+    const digits =[];
+
+    for(let i = 1; i < 10; i++ ){
+      digits.push(
+        <button key={i} onClick={()=>updateOutput(i.toString())}>{i}</button>
+      )
+    }
+    return digits;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className = "calculator">
+          <div className = 'display'>
+            { result ? <span> ({result}) </span> : '' } { output || '0' }
+          </div>
+          <div className = 'operators'>
+            <button onClick={()=>updateOutput('/')}>/</button>
+            <button onClick={()=>updateOutput('*')}>*</button>
+            <button onClick={()=>updateOutput('+')}>+</button>
+            <button onClick={()=>updateOutput('-')}>-</button>
+            <button onClick={answer}>Ans</button>
+            <button onClick={clear}>Clear</button>
+            <button onClick={deleteOutput}>Del</button>
+
+          </div>
+          <div className='digits'>
+            {createDigits()}
+            <button onClick={()=>updateOutput('0')}>0</button>
+            <button onClick={()=>updateOutput('.')}>.</button>
+            <button onClick={showResult}>=</button>
+          </div>
+      </div>
     </div>
   );
 }
